4b9d387d8b47052570ad6bd74f2893a5a4ef41f5
Lucas Segurado <lsegurado1996@gmail.com>
2024-07-27 23:31:51 +0200

    Type fix

--- src/stories/Preview.ts
+++ src/stories/Preview.ts
@@ -1,4 +1,13 @@
-import type { BaseAnnotations, Renderer, Args } from "@storybook/types";
+import type { BaseAnnotations, Renderer, Args, Canvas } from "@storybook/types";
 
-export type MichiRenderer = Record<keyof Renderer, Element>;
+export interface MichiRenderer extends Renderer {
+  /** What is the type of the `component` annotation in this renderer? */
+  component: Element;
+  /** What does the story function return in this renderer? */
+  storyResult: Element;
+  /** What type of element does this renderer render to? */
+  canvasElement: Element;
+  mount(): Promise<Canvas>;
+  T?: Element;
+}
 export type Preview<TArgs = Args> = BaseAnnotations<MichiRenderer, TArgs>;
